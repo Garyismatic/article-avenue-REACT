2a88c5c8b0211fb3927c84ae2d5c43a767a676a6
Garyismatic <garyevans1501@gmail.com>
2024-08-07 13:02:10 +0100

    enabled a set user to post comments to an article

--- src/Api.js
+++ src/Api.js
@@ -25,8 +25,15 @@ export const updateArticleVotes = (article_id, votes) => {
 };
 
 export const getArticleComments = (article_id) => {
-  return api.get(`/articles/${article_id}/comments`).then(({data}) => {
-    return data.comments
-  })
-}
+  return api.get(`/articles/${article_id}/comments`).then(({ data }) => {
+    return data.comments;
+  });
+};
 
+export const postComment = (article_id, user, comment) => {
+  return api
+    .post(`/articles/${article_id}/comments`, { username: user, body: comment })
+    .then(({ data }) => {
+      return data.commentPosted;
+    });
+};
--- src/components/ArticleActions.jsx
+++ src/components/ArticleActions.jsx
@@ -6,10 +6,12 @@ import "./CSS-modules/articleActions.css";
 import { useState } from "react";
 import { updateArticleVotes } from "../Api";
 import { useNavigate } from "react-router-dom";
+import CommentInput from "./CommentInput";
 
 function ArticleActions(props) {
   const { article, article_id } = props;
   const [voteCount, setVoteCount] = useState(0);
+  const [commentOpen, setCommentOpen] = useState(false)
   const navigate = useNavigate()
 
   const handleUpVote = () => {
@@ -34,6 +36,11 @@ function ArticleActions(props) {
     });
   };
 
+  const handleClick = () => {
+    setCommentOpen((currCommentOpen) => {
+      return !currCommentOpen
+    })
+  }
 
   return (
     <>
@@ -52,11 +59,12 @@ function ArticleActions(props) {
           </IconButton>
         </section>
         <section className="article-comments">
-          <Button variant="outlined" endIcon={<CommentIcon />}>
+          <Button variant="outlined" endIcon={<CommentIcon />} onClick={handleClick}>
            Add Comment
           </Button>
         </section>
       </div>
+        <CommentInput commentOpen={commentOpen} setCommentOpen={setCommentOpen} />
     </>
   );
 }
