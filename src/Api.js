66059616116e2272343e2e3b6a5bce347de27ace
Gary <garyevans1501@gmail.com>
2025-07-17 12:14:35 +0100

    edit baseURL

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://article-avenue-api.onrender.com/api",
+  baseURL: "https://article-avenue-api.onrender.com/api/",
 });
 
 export const getArticles = (topic, sort_by, order) => {
